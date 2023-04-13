b44dee73b5f2b80a202065e01570cfadab549901
Christian Froehlich <christian.froehlich@dedalus.eu>
2023-04-13 09:56:09 +0200

    test: add DSTTest

---
+++ src/test/java/org/time/dst/DSTTest.java
@@ -0,0 +1,96 @@
+package org.time.dst;
+
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class DSTTest {
+    @Test
+    public void testConvertInstantToZonedDateTime() {
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-04-10T06:00:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-11-10T06:00:00Z"), ZoneId.of("Europe/Berlin")));
+
+        // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-03-26T00:59:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-03-26T01:00:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-03-26T01:01:00Z"), ZoneId.of("Europe/Berlin")));
+
+        ZonedDateTime start = ZonedDateTime.ofInstant(Instant.parse("2023-03-26T00:59:00Z"), ZoneId.of("Europe/Berlin"));
+        ZonedDateTime end = ZonedDateTime.ofInstant(Instant.parse("2023-03-26T01:00:00Z"), ZoneId.of("Europe/Berlin"));
+        System.out.println(Duration.between(start, end).toMinutes());
+
+        // switch from summer to winter in zone Europe/Berlin: 2023-10-29T03:00 -> 2023-10-29T02:00
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-10-29T00:59:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-10-29T01:00:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-10-29T01:01:00Z"), ZoneId.of("Europe/Berlin")));
+
+        start = ZonedDateTime.ofInstant(Instant.parse("2023-10-29T00:59:00Z"), ZoneId.of("Europe/Berlin"));
+        end = ZonedDateTime.ofInstant(Instant.parse("2023-10-29T01:00:00Z"), ZoneId.of("Europe/Berlin"));
+        System.out.println(Duration.between(start, end).toMinutes());
+    }
+
+    // start and end in the same time offset
+    @Test
+    public void example_1() {
+        // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-04-10T08:00+02:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-04-10T12:00+02:00[Europe/Berlin]");
+        System.out.println(String.format("ZonedDateTime '%s' to '%s' and duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+
+        // switch from summer to winter in zone Europe/Berlin: 2023-10-29T03:00 -> 2023-10-29T02:00
+        start = ZonedDateTime.parse("2023-11-10T08:00+01:00[Europe/Berlin]");
+        end = ZonedDateTime.parse("2023-11-10T12:00+01:00[Europe/Berlin]");
+        System.out.println(String.format("ZonedDateTime '%s' to '%s' and duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+    }
+
+    // start in winter time and end in summer time
+    @Test
+    public void example_2() {
+        // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-03-26T01:00+01:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-03-26T04:00+02:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+
+        start = ZonedDateTime.parse("2023-03-26T00:00+01:00[Europe/Berlin]");
+        end = ZonedDateTime.parse("2023-03-27T00:00+02:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in hours: %d", start, end, Duration.between(start, end).toHours()));
+    }
+
+    // start in summer time and end in winter time
+    @Test
+    public void example_3() {
+        // switch from summer to winter in zone Europe/Berlin: 2023-10-29T03:00 -> 2023-10-29T02:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-10-29T01:00+02:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-10-29T04:00+01:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+
+        start = ZonedDateTime.parse("2023-10-29T00:00+02:00[Europe/Berlin]");
+        end = ZonedDateTime.parse("2023-10-30T00:00+01:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in hours: %d", start, end, Duration.between(start, end).toHours()));
+    }
+
+    //  end time in the gap from winter time to summer time
+    @Test
+    public void example_4() {
+        // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-03-26T01:00+01:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-03-26T02:30+01:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+    }
+
+    // end time in the gap from summer time to winter time
+    @Test
+    public void example_5() {
+        // switch from summer to winter in zone Europe/Berlin: 2023-10-29T03:00 -> 2023-10-29T02:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-10-29T01:00+02:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-10-29T02:30+02:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+
+        start = ZonedDateTime.parse("2023-10-29T01:00+02:00[Europe/Berlin]");
+        end = ZonedDateTime.parse("2023-10-29T02:30+01:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+    }
+}
--- src/test/java/org/time/dst/DaylightSavingTimeTest.java
+++ src/test/java/org/time/dst/DaylightSavingTimeTest.java
@@ -8,6 +8,63 @@ import java.time.temporal.ChronoUnit;
 public class DaylightSavingTimeTest {
 
     @Test
+    public void testPrintInstantToZonedDateTime() {
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-04-10T06:00:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-11-10T06:00:00Z"), ZoneId.of("Europe/Berlin")));
+        // output
+        // 2023-04-10T08:00+02:00[Europe/Berlin]
+        // 2023-11-10T07:00+01:00[Europe/Berlin]
+
+        // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-03-26T00:59:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-03-26T01:00:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-03-26T01:01:00Z"), ZoneId.of("Europe/Berlin")));
+        // output
+        // 2023-03-26T01:59+01:00[Europe/Berlin]
+        // 2023-03-26T03:00+02:00[Europe/Berlin]
+        // 2023-03-26T03:01+02:00[Europe/Berlin]
+
+        ZonedDateTime start = ZonedDateTime.ofInstant(Instant.parse("2023-03-26T00:59:00Z"), ZoneId.of("Europe/Berlin"));
+        ZonedDateTime end = ZonedDateTime.ofInstant(Instant.parse("2023-03-26T01:00:00Z"), ZoneId.of("Europe/Berlin"));
+        System.out.println(Duration.between(start, end).toMinutes());
+        // output
+        // 1
+
+        // switch from summer to winter in zone Europe/Berlin: 2023-10-29T03:00 -> 2023-10-29T02:00
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-10-29T00:59:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-10-29T01:00:00Z"), ZoneId.of("Europe/Berlin")));
+        System.out.println(ZonedDateTime.ofInstant(Instant.parse("2023-10-29T01:01:00Z"), ZoneId.of("Europe/Berlin")));
+        // output
+        // 2023-10-29T02:59+02:00[Europe/Berlin]
+        // 2023-10-29T02:00+01:00[Europe/Berlin]
+
+        start = ZonedDateTime.ofInstant(Instant.parse("2023-10-29T00:59:00Z"), ZoneId.of("Europe/Berlin"));
+        end = ZonedDateTime.ofInstant(Instant.parse("2023-10-29T01:00:00Z"), ZoneId.of("Europe/Berlin"));
+        System.out.println(Duration.between(start, end).toMinutes());
+        // output
+        // 1
+    }
+
+    @Test
+    public void testPrintZonedDateTimeInWinterTime() {
+        // switch from summer to winter in zone Europe/Berlin: 2023-10-29T03:00 -> 2023-10-29T02:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-11-10T08:00+01:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-11-10T12:00+01:00[Europe/Berlin]");
+        System.out.println(String.format("ZonedDateTime '%s' to '%s' and duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+//        output:
+//        ZonedDateTime '2023-11-10T08:00+01:00[Europe/Berlin]' to '2023-11-10T12:00+01:00[Europe/Berlin]' and duration in minutes: 240
+    }
+    @Test
+    public void testPrintZonedDateTimeInSummerTime() {
+        // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-04-10T08:00+02:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-04-10T12:00+02:00[Europe/Berlin]");
+        System.out.println(String.format("ZonedDateTime '%s' to '%s' and duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+//        output:
+//        ZonedDateTime '2023-04-10T08:00+02:00[Europe/Berlin]' to '2023-04-10T12:00+02:00[Europe/Berlin]' and duration in minutes: 240
+    }
+
+    @Test
     public void testPrintInstantAndZonedDateTimeAroundWinterToSummerTimeSwitch() {
         // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
         Instant utcStart = Instant.parse("2023-03-26T00:59:00Z");
@@ -47,6 +104,62 @@ public class DaylightSavingTimeTest {
             System.out.println(String.format("'%s' := '%s'", utcStart, utcStart.atZone(ZoneId.of("Europe/Berlin"))));
             utcStart = utcStart.plus(1, ChronoUnit.MINUTES);
         }
+    }
+
+    @Test
+    public void testPrintDurationWithStartInWinterAndEndInSummerTime() {
+        // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-03-26T01:00+01:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-03-26T04:00+02:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+
+        start = ZonedDateTime.parse("2023-03-26T00:00+01:00[Europe/Berlin]");
+        end = ZonedDateTime.parse("2023-03-27T00:00+02:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in hours: %d", start, end, Duration.between(start, end).toHours()));
+
+    }
+
+    @Test
+    public void testPrintDurationWithStartInSummerAndEndInWinterTime() {
+        // switch from summer to winter in zone Europe/Berlin: 2023-10-29T03:00 -> 2023-10-29T02:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-10-29T01:00+02:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-10-29T04:00+01:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+
+        start = ZonedDateTime.parse("2023-10-29T00:00+02:00[Europe/Berlin]");
+        end = ZonedDateTime.parse("2023-10-30T00:00+01:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in hours: %d", start, end, Duration.between(start, end).toHours()));
+
+    }
+
+    @Test
+    public void testPrintDurationWithStartInWinterAndEndInSwitchHour() {
+        // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-03-26T01:00+01:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-03-26T02:30+01:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+
+        start = ZonedDateTime.parse("2023-03-26T01:00+01:00[Europe/Berlin]");
+        end = ZonedDateTime.parse("2023-03-26T02:30+02:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+
+    }
+
+    @Test
+    public void obelix() {
+        LocalDateTime localDateTime = LocalDateTime.parse("2023-03-26T04:00");
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.of("Europe/Berlin"));
+        System.out.println(localDateTime);
+        System.out.println(zonedDateTime);
+    }
+
+    @Test
+    public void testPrintDurationWithStartInWinterTime() {
+        // switch from winter to summer in zone Europe/Berlin: 2023-03-26T02:00 -> 2023-03-26T03:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-03-26T01:00+01:00[Europe/Berlin]");
+        Duration duration = Duration.ofMinutes(90);
+        ZonedDateTime end = start.plus(duration);
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
     }
 
     @Test
@@ -98,6 +211,14 @@ public class DaylightSavingTimeTest {
     }
 
     @Test
+    public void test() {
+        // switch from summer to winter in zone Europe/Berlin: 2023-10-29T03:00 -> 2023-10-29T02:00
+        ZonedDateTime start = ZonedDateTime.parse("2023-10-29T01:00+02:00[Europe/Berlin]");
+        ZonedDateTime end = ZonedDateTime.parse("2023-10-29T02:30+01:00[Europe/Berlin]");
+        System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
+    }
+
+    @Test
     public void testPrintDurationWithStartInForstHourAndEndInAfterSummerToWinter() {
         // switch from summer to winter in zone Europe/Berlin: 2023-10-29T03:00 -> 2023-10-29T02:00
         // first hour is the summer time hour:  2023-10-29T02:00+02:00[Europe/Berlin] until 2023-10-29T03:00+02:00[Europe/Berlin]
@@ -107,5 +228,27 @@ public class DaylightSavingTimeTest {
             System.out.println(String.format("Start '%s' stop '%s' duration in minutes: %d", start, end, Duration.between(start, end).toMinutes()));
             start = start.plus(1, ChronoUnit.MINUTES);
         }
+    }
+
+
+    @Test
+    public void blafasel() {
+//        Instant instant = Instant.parse("2023-03-26T05:00:00Z");
+//
+//        ZoneId zoneId = ZoneId.of("Europe/Berlin");
+//        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, zoneId);
+//        boolean isDST = zoneId.getRules().isDaylightSavings(instant);
+//        Duration durationDST = zoneId.getRules().getDaylightSavings(instant);
+//
+//        System.out.println(instant);
+//        System.out.println(zonedDateTime);
+//        System.out.println(isDST);
+//        System.out.println(durationDST.toMinutes());
+//
+//        System.out.println(zoneId.getRules().getTransitionRules().get(0));
+
+        ZonedDateTime start = ZonedDateTime.parse("2023-10-29T01:00+02:00[Europe/Berlin]");
+        Duration duration = Duration.ofMinutes(120);
+        System.out.println(String.format("Start '%s' plus duration in minutes '%d' results in '%s'", start, duration.toMinutes(), start.plus(duration)));
     }
 }
